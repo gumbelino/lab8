61e94ab604c986ea68cbd9b64d1e880d266ce8fe
Gustavo Umbelino <gumbelin@ucsd.edu>
2018-03-01 10:12:09 -0800

    trying camera

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -3,21 +3,30 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
   initializePage();
-})
+});
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	console.log("Page ready");
- 	initCamera();
- 	initGestures();
- 	initRSVPForm();
+  console.log("Page ready");
+  initCamera();
+  initGestures();
+  initRSVPForm();
 }
 
-// init jQuery gestures  
+// init jQuery gestures
 function initGestures() {
-	// add gestures listener here
+  //console.log("HI!!@@");
+  // add gestures listener here
+  $(function() {
+    $(".judge-img").bind("taphold", tapholdHandler);
+    function tapholdHandler(event) {
+      var targetIDPrefix = event.target.id;
+      console.log("got prefix: " + targetIDPrefix);
+      $("#" + targetIDPrefix + "-bio").show();
+    }
+  });
 }
 
 // init RSVP form submit listener
